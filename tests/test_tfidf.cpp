c5c2bf20174626033d17d11de9854bb4a5be0296
raymondngiam <ha.ngiam87@gmail.com>
2021-05-11 00:23:24 +0800

    Add SingleTransform test for Tfidf lib

--- tests/test_tfidf.cpp
+++ tests/test_tfidf.cpp
@@ -25,15 +25,29 @@ cv::Mat hist_result = (cv::Mat_<double>(4,5)<<\
     -0.0956, 0., -0.0319, 0.    , 0.1980,\
     -0.0558, 0., -0.0558, 0.    , 0.     \
   );
+Tfidf tfidf(M);
 }  // namespace
 
 TEST(Tfidf, ComputeHistogram) {
-  Tfidf tfidf(M);
   auto hist_final = tfidf.get_full_histograms();
   std::cout<<hist_result<<std::endl;
   EXPECT_TRUE(mat_almost_equal<double>(hist_final,hist_result,1e-4))      
       << "target histogram:\n"
       << hist_result << "\ncomputed histogram:\n"
       << hist_final;
+}
+
+TEST(Tfidf, SingleTransform) {
+  cv::Mat M2 = (cv::Mat_<uint16_t>(1,5)<<\
+    5,2,1,0,0
+  );
+  cv::Mat M2_result = (cv::Mat_<double>(1,5)<<\
+    -0.1395, 0., -0.0279, 0.    , 0.    
+  );
+  cv::Mat trans = tfidf.transform_hist(M2);
+  EXPECT_TRUE(mat_almost_equal<double>(trans,M2_result,1e-4))      
+      << "target histogram:\n"
+      << M2_result << "\ncomputed histogram:\n"
+      << trans;
 }
 
